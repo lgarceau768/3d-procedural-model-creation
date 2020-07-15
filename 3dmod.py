c073d8ac50bd0586c40ab0110c47be3ef64546df
lgarceau768 <luke.garceau@kavokerr.com>
2020-07-15 13:39:14 -0400

    Created the sphere method

--- 3dmod.py
+++ 3dmod.py
@@ -11,6 +11,17 @@ class customObj:
     def getName(self):
         return self.name
 
+class customBody:
+    def __init__(self, list, name):
+        self.list = list
+        self.name = name
+    
+    def getName(self):
+        return self.name
+
+    def getBodyParts(self):
+        return self.list
+
 def createBox(name, posVector, rotationVector, l, w, h):
     '''
     position and rotation starts at 0,0,0 x,y,z
@@ -19,5 +30,21 @@ def createBox(name, posVector, rotationVector, l, w, h):
     model = box(pos=posVector, axis=rotationVector, length=l, width=w, height=h)
     return customObj(model, name)
 
-armSegment = createBox("arm1", vector(-3,0,-2), vector(3,3,0), 0.5, 0.5, 6)
-print(armSegment.getName())
+def createSphere(name, posVector, r):
+    '''
+    position is based of x,y,z
+    '''
+    model = sphere(pos=posVector, radius=r)
+    return customObj(model, name)
+
+def createBodyList(name, args):
+    body = []
+    for arg in args:
+        body.append(args)
+    return customBody(name, body)
+
+
+armSegment = createBox("arm1", vector(-3,0,-2), vector(0,0,0), 0.5, 0.5, 6)
+sphereSegment = createSphere("head", vector(-3, 3.5, -2), 0.5)
+body1 = createBodyList("body1", [armSegment, sphere])
+
--- README.md
+++ README.md
@@ -5,9 +5,13 @@ Dependencies
 Todo:
 - create functions
   - ~~make box~~
-  - make sphere
+  - ~~make sphere~~
 - location data
   - research ecosystems and how it affects wildlife
 - create base "monster" with simple boxes
 - work to automate the above functionality and add randomness 
 - incorporate ecosystem features into monster creation ^
+
+
+Ideas:
+- could use the spheres as joints and then create movement from there (dont really need that rn for a poc but could explore in future)
